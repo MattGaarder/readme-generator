f71096735140ac4e2cf92725ee2c9ebdb60a3ea3
Matt Gaarder <matteus.gaarder@gmail.com>
2023-04-19 09:44:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const fs = require("fs");
-// const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-const heya = require("./folder1/index")
-// array of questions for user
-
-
-// function to initialize program
-function init() {
+const heya = require("./folder1/index") // array of questions for user
+function init() { // function to initialize inquirer and generate markdown document
     inquirer.prompt(heya).then(answer => {
         console.log(answer);
         fs.writeFile("./folder1/README.md", generateMarkdown(answer), err => console.log(err))
     })
 }
-
-// function call to initialize program
 init();
